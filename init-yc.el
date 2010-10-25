1d6bcd30e0bd200891f7aeb09c67cafd4535d01e
Sugano Yoshihisa(E) <koshian@trans-nt.com>
2010-10-25 23:33:09 +0700

    fix yc load condition

--- init-yc.el
+++ init-yc.el
@@ -1,9 +1,10 @@
-(and (require 'yc nil t)
-     (global-yc-mode 1)
-     (global-set-key "\C-o" 'canna-toggle-japanese-mode)
-     (global-set-key "\C-o" 'yc-input-mode)
-     (setq yc-use-color nil)
-     (define-key yc-edit-mode-map "\C-h" 'yc-edit-backward-delete-char))
+(load "yc")
+(global-yc-mode 1)
+(global-set-key "\C-o" 'canna-toggle-japanese-mode)
+(global-set-key "\C-o" 'yc-input-mode)
+(setq yc-use-color nil)
+(define-key yc-edit-mode-map "\C-h" 'yc-edit-backward-delete-char)
+
 
 
 
