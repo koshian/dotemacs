6757d3f2629f521ea446211f666e1b17872e124a
Sugano Yoshihisa(E) <koshian@foxking.org>
2013-02-14 21:59:08 +0700

    Check locate "inf-ruby"

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -28,10 +28,11 @@
       (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
                                      interpreter-mode-alist))
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-            (inf-ruby-keys)))
+(when (locate-library "inf-ruby")
+  (autoload 'run-ruby "inf-ruby"
+    "Run an inferior Ruby process")
+  (autoload 'inf-ruby-keys "inf-ruby"
+    "Set local key defs for inf-ruby in ruby-mode")
+  (add-hook 'ruby-mode-hook
+            '(lambda ()
+               (inf-ruby-keys))))
