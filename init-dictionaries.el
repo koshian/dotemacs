20c063dbaf947e278d4a98070c43d701041830f8
Sugano Yoshihisa(E) <koshian@foxking.org>
2013-11-07 23:10:25 +0900

    Add dictionary()

--- init-dictionaries.el
+++ init-dictionaries.el
@@ -19,3 +19,30 @@
 ;; 最大表示数
 (setq lookup-max-hits 256)
 
+;; Mac Dictionaries
+;; from http://sakito.jp/mac/dictionary.html
+(defun dictionary ()
+  "dictionary.app"
+  (interactive)
+
+  (let ((editable (not buffer-read-only))
+        (pt (save-excursion (mouse-set-point last-nonmenu-event)))
+        beg end)
+
+    (if (and mark-active
+             (<= (region-beginning) pt) (<= pt (region-end)) )
+        (setq beg (region-beginning)
+              end (region-end))
+      (save-excursion
+        (goto-char pt)
+        (setq end (progn (forward-word) (point)))
+        (setq beg (progn (backward-word) (point)))
+        ))
+
+    (browse-url
+     (concat "dict:///"
+             (url-hexify-string (buffer-substring-no-properties beg end))))))
+
+(when (eq window-system 'ns)
+  (define-key global-map (kbd "C-x C-i") 'dictionary))
+
