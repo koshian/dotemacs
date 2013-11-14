d2f5a1471cfdae3653bf0d377360ced797ef34a5
Sugano Yoshihisa(E) <koshian@foxking.org>
2013-11-14 09:14:13 +0900

    Using ssh automaster with tramp

--- init-tramp.el
+++ init-tramp.el
@@ -1,1 +1,5 @@
 (setq tramp-default-method "ssh")
+(setq tramp-ssh-controlmaster-options
+      (concat
+       "-o ControlPath=/tmp/%%r@%%h:%%p "
+       "-o ControlMaster=auto -o ControlPersist=yes"))
