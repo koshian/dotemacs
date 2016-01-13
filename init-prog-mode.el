85bcfa649ff9c0fc1f77effdb420e8225bc6b126
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-14 01:55:47 +0900

    Added init-prog-mode.el Added emoji packages.

---
+++ init-prog-mode.el
@@ -0,0 +1,41 @@
+;;; init-prog-mode.el --- Settings for several programming modes.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Sugano `Koshian' Yoshihisa(E)
+
+;; Author: Sugano `Koshian' Yoshihisa(E) <koshian@foxking.org>
+;; Keywords: c
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(global-set-key [(super n)] 'flycheck-next-error)
+(global-set-key [(super p)] 'flycheck-previous-error)
+
+;; codesaerch.el for google code search
+(require 'codesearch)
+(codesearch-build-index (expand-file-name "~/var/codesearch"))
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(provide 'init-prog-mode)
+;;; init-prog-mode.el ends here
