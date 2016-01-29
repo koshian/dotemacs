e537599897827223d0f076fdf0ac21c0432ba0ca
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-29 15:48:43 +0900

    Fix js-mode

--- init-js-mode.el
+++ init-js-mode.el
@@ -1,4 +1,10 @@
-  (autoload 'js-mode "js" "JavaScript" t)
-  (setq auto-mode-alist
-	(cons
-	 '("\\.js$" . js-mode) auto-mode-alist))
+(autoload 'js-mode "js" "JavaScript" t)
+(setq auto-mode-alist
+      (cons
+       '("\\.js$" . js-mode) auto-mode-alist))
+
+(if (locate-library "auto-complete")
+    (add-hook 'js-mode-hook
+              (lambda ()
+                (add-to-list 'ac-sources 'ac-source-abbrev)
+                (load-auto-complete))))
