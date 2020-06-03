5537b70ba021f4afb535c60101d0dc63eeb702f8
Sugano `Koshian' Yoshihisa(E) <koshian@foxking.org>
2020-06-03 23:46:38 +0900

    update init-php-mode

--- init-php-mode.el
+++ init-php-mode.el
@@ -2,14 +2,37 @@
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
+
+
 (add-hook 'php-mode-hook
-          '(lambda ()
-             (progn
-               (c-set-style "stroustrup")
-               (setq indent-tabs-mode t)
-               (setq tab-width 8)
-               (setq c-basic-offset 8)
-               (setq standard-indent 8))))
+          (lambda ()
+            ; (c-set-style "linux")
+            (setq php-mode-force-pear t)
+            (setq tab-width 4
+                  c-basic-offset 4
+                  standard-indent 4
+                  c-hanging-comment-ender-p nil
+                  indent-tabs-mode nil)))
+
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+
+;          (setq php-mode-force-pear t)
+;            (setq tab-width 4)
+;            (setq c-basic-offset 4)
+;            (setq indent-tabs-mode nil)))
+
+             ; (php-set-style "pear)
+
 ;; (add-hook 'php-mode-hook
 ;;           '(lambda ()
 ;;              (progn
